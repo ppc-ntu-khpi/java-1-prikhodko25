58c724c958d29703c7b77178e6e981d66d3e37e4
Приходько Софія <sofiykapiryet@gmail.com>
2026-02-01 15:58:06 +0200

    done

--- src/test/CustomerTest.java
+++ src/test/CustomerTest.java
@@ -1,1 +1,19 @@
+package test;
 
+import domain.Customer;
+
+public class CustomerTest {
+    public static void main(String[] args) {
+        Customer myCustomer = new Customer();
+
+        System.out.println("Default customer data:");
+        myCustomer.displayCustomerInfo();
+
+        myCustomer.setID(101);
+        myCustomer.setStatus(false);
+        myCustomer.setTotal(5500.75);
+
+        System.out.println("Updated customer data:");
+        myCustomer.displayCustomerInfo();
+    }
+}
